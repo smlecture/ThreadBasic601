22feb8911d11f918ceec63b258292a9b53e35e17
H-J Yoo <yoohj@smu.ac.kr>
2021-06-01 23:47:12 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/company/threadbasic601/MainActivity.java
@@ -0,0 +1,62 @@
+package kr.co.company.threadbasic601;
+
+import androidx.annotation.WorkerThread;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    WorkerThread wt;
+    boolean running = true;
+
+    String TAG = "THREAD";
+
+    class WorkerThread extends Thread {
+        public void run() {
+            int i = 0;
+            for (i = 0; i < 20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                Log.v(TAG, "Thread time=" + i);
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG, "Now I am in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        running = true;
+        wt = new WorkerThread();
+        wt.start();
+//        new WorkerThread().start();
+        Log.v(TAG, "Now I am in onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        running = false;
+        Log.v(TAG, "Now I am in onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "Now I am in onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "Now I am in onResume");
+    }
+}
